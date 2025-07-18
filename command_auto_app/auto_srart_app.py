7d19266fa7025124dbc799c901624887b0c760b8
dali <@dali>
2025-07-18 21:34:05 +0800

    feat(command_auto_app): 实现自动启动应用功能
    - 新增 WorkerThread 类用于执行启动应用的任务- 更新 MainWindow 类，添加启动和停止任务的功能
    - 修改 app_path.ini 文件，更新应用路径配置

--- command_auto_app/auto_srart_app.py
+++ command_auto_app/auto_srart_app.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from PySide6.QtCore import QThread, Signal, QMutex
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QTextEdit, QComboBox,QLabel,QSpinBox, QGridLayout,
@@ -25,6 +27,58 @@ def read_config(app_path):
 #         config.set('app启动路径', key, value)
 #     with open(app_path, 'w') as f:
 #         config.write(f)
+
+
+class WorkerThread(QThread):
+    # 使用 pyside6.QrCore.Signal 创建线程 用于向主线程发送信号
+    message_signal = Signal(str)
+
+    def __init__(self, music_path=None, idea_path=None):
+        super().__init__()
+        self.mutex = QMutex()
+        self.is_running = True
+
+        self.music_path = music_path
+        self.idea_path = idea_path
+
+
+
+    def start_app(self, app_path, app_name):
+        try:
+            subprocess.Popen(app_path)
+            self.message_signal.emit(f"{app_name}启动成功")
+        except Exception as e:
+            self.message_signal.emit(f"Error: {e},{app_name}路径是否正确: {app_path}")
+
+
+    def run(self):
+        """ 执行工作线程 """
+        self.mutex.lock()
+        running = self.is_running
+        self.mutex.unlock()
+        if not running:
+            return
+
+        if self.music_path:
+            self.message_signal.emit(f'正在打开 music')
+            self.start_app(self.music_path, "music")
+
+        for _ in range(3):
+            self.mutex.lock()
+            running = self.is_running
+            self.mutex.unlock()
+            if not running:
+                return
+            time.sleep(1)
+
+        if self.idea_path:
+            self.message_signal.emit(f'正在打开 idea')
+            self.start_app(self.idea_path, "idea")
+
+    def stop(self):
+        self.mutex.lock()
+        self.is_running = False
+        self.mutex.unlock()
 
 
 
@@ -73,9 +127,9 @@ class MainWindow(QMainWindow):
         self.open_idea.setChecked(True)
         grid.addWidget(self.open_idea, 1, 0)
 
-        self.open_idea = QLineEdit()
-        self.open_idea.setText(os.path.join(start_path['open_idea']))
-        grid.addWidget(self.open_idea, 1, 1)
+        self.idea_path  = QLineEdit()
+        self.idea_path.setText(os.path.join(start_path['idea']))
+        grid.addWidget(self.idea_path, 1, 1)
 
         # 绑容定网格布局到垂直布局中
         v_layout.addLayout(grid)
@@ -85,11 +139,11 @@ class MainWindow(QMainWindow):
         h_layout = QHBoxLayout()
 
         self.start_button = QPushButton('开始')
-        # self.start_button.clicked.connect(self.start)
+        self.start_button.clicked.connect(self.start)
         h_layout.addWidget(self.start_button)
 
         self.pause_button = QPushButton('结束')
-        # self.pause_button.clicked.connect(self.pause)
+        self.pause_button.clicked.connect(self.pause)
         h_layout.addWidget(self.pause_button)
 
         self.save_change_button = QPushButton('保存更改')
@@ -114,6 +168,45 @@ class MainWindow(QMainWindow):
         container.setLayout(v_layout)
         # 设置窗口的容器
         self.setCentralWidget(container)
+
+        # 创建变量，用来保持 worker_thread 工作线程
+        self.worker_thread = None
+    def start(self):
+        if self.worker_thread is None or not self.worker_thread.isRunning():
+
+            music_path = None
+            if self.open_music.isChecked():
+                self.output_text.append('# --> 待执行任务：打开音乐软件')
+                music_path = self.music_path.text()
+
+            idea_path = None
+            if self.open_idea.isChecked():
+                self.output_text.append('# --> 待执行任务：打开编译器')
+                idea_path = self.idea_path.text()
+
+
+            if not music_path or not idea_path:
+                self.output_text.append('# --> 错误：请选择要启动的程序')
+                return
+            else:
+                self.worker_thread = WorkerThread(music_path, idea_path)
+                self.worker_thread.message_signal.connect(self.update_output)
+                self.worker_thread.start()
+
+    def pause(self):
+        if self.worker_thread is None:
+            self.output_text.append('请先开始任务！！！')
+            return
+
+        if self.worker_thread and self.worker_thread.isRunning():
+            self.worker_thread.stop()
+            self.output_text.append('停止任务...')
+
+    def update_output(self, message):
+        self.output_text.append(message)
+
+
+
 
 
 
