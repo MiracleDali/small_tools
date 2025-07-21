ca8a7a2387f7c1724b7d3b74e1700efb7f252093
dali <@dali>
2025-07-21 19:49:45 +0800

    feat(command_auto_app): 增加翻译软件启动功能并优化配置保存
    - 在 app_path.ini 中添加翻译软件路径和默认启动状态配置
    - 修改 auto_srart_app.py 以支持翻译软件启动- 实现配置保存功能，包括软件路径和默认启动状态
    - 优化用户界面，增加翻译软件启动选项
    - 调整输出提示信息，提高用户体验

--- command_auto_app/auto_srart_app.py
+++ command_auto_app/auto_srart_app.py
@@ -1,45 +1,37 @@
 import subprocess
-
 from PySide6.QtCore import QThread, Signal, QMutex
 from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QPushButton, QTextEdit, QComboBox,QLabel,QSpinBox, QGridLayout,
+    QApplication, QMainWindow, QPushButton, QTextEdit, QLabel, QGridLayout,
     QVBoxLayout, QWidget, QLineEdit, QHBoxLayout,QCheckBox)
-import pyautogui
-from datetime import datetime
 import time
 import os
-import random
 from configparser import RawConfigParser
 
 
 path = os.path.dirname(__file__)
-app_start_file_path = os.path.join(path, 'app_path.ini')
+app_config_file_path = os.path.join(path, 'app_path.ini')
 
-def read_config(app_path):
+def read_config(app_paths):
+    """
+    读取software的路径以供打开软件使用
+    """
     config = RawConfigParser()
-    config.read(app_path, encoding='utf-8')
-    return config['app启动路径']
-
-# def write_config(app_path, app_path_dict):
-#     config = RawConfigParser()
-#     config.add_section('app启动路径')
-#     for key, value in app_path_dict.items():
-#         config.set('app启动路径', key, value)
-#     with open(app_path, 'w') as f:
-#         config.write(f)
+    config.read(app_paths, encoding='utf-8')
+    return config
 
 
 class WorkerThread(QThread):
     # 使用 pyside6.QrCore.Signal 创建线程 用于向主线程发送信号
     message_signal = Signal(str)
 
-    def __init__(self, music_path=None, idea_path=None):
+    def __init__(self, music_path=None, idea_path=None, translate_path=None):
         super().__init__()
         self.mutex = QMutex()
         self.is_running = True
 
         self.music_path = music_path
         self.idea_path = idea_path
+        self.translate_path = translate_path
 
 
 
@@ -75,6 +67,20 @@ class WorkerThread(QThread):
             self.message_signal.emit(f'正在打开 idea')
             self.start_app(self.idea_path, "idea")
 
+        for _ in range(3):
+            self.mutex.lock()
+            running = self.is_running
+            self.mutex.unlock()
+            if not running:
+                return
+            time.sleep(1)
+
+        if self.translate_path:
+            self.message_signal.emit(f'正在打开 translate')
+            self.start_app(self.translate_path, "translate")
+
+        self.message_signal.emit(f'软件打开完成!!!')
+
     def stop(self):
         self.mutex.lock()
         self.is_running = False
@@ -86,8 +92,10 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # 获取应用的启动程序路径
-        start_path = read_config(app_start_file_path)
+        # 获取应用的启动程序路径和是否打开的default
+        start_path = read_config(app_config_file_path)['app启动路径']
+        default_status = read_config(app_config_file_path)['app是否默认启动']
+
 
         # 设置窗口标题
         self.setWindowTitle("看!!!打工人来了")
@@ -116,7 +124,7 @@ class MainWindow(QMainWindow):
 
         # 设置需要启动的应用选项
         self.open_music = QCheckBox('Open Music')
-        self.open_music.setChecked(True)
+        self.open_music.setChecked(True if default_status['music'] == 'True' else False)
         grid.addWidget(self.open_music, 0, 0)
 
         self.music_path = QLineEdit()
@@ -124,12 +132,20 @@ class MainWindow(QMainWindow):
         grid.addWidget(self.music_path, 0, 1)
 
         self.open_idea = QCheckBox('open_idea')
-        self.open_idea.setChecked(True)
+        self.open_idea.setChecked(True if default_status['idea'] == 'True' else False)
         grid.addWidget(self.open_idea, 1, 0)
 
         self.idea_path  = QLineEdit()
         self.idea_path.setText(os.path.join(start_path['idea']))
         grid.addWidget(self.idea_path, 1, 1)
+
+        self.open_translate = QCheckBox('open_translate')
+        self.open_translate.setChecked(True if default_status['translate'] == 'True' else False)
+        grid.addWidget(self.open_translate, 2, 0)
+
+        self.translate_path = QLineEdit()
+        self.translate_path.setText(os.path.join(start_path['translate']))
+        grid.addWidget(self.translate_path, 2, 1)
 
         # 绑容定网格布局到垂直布局中
         v_layout.addLayout(grid)
@@ -147,7 +163,7 @@ class MainWindow(QMainWindow):
         h_layout.addWidget(self.pause_button)
 
         self.save_change_button = QPushButton('保存更改')
-        # self.save_change_button.clicked.connect(self.reset)
+        self.save_change_button.clicked.connect(self.save_change)
         h_layout.addWidget(self.save_change_button)
 
         # 将水平布局添加到垂直布局
@@ -156,9 +172,9 @@ class MainWindow(QMainWindow):
         # 创建文本框_用于显示输出
         self.output_text = QTextEdit()
         self.output_text.setReadOnly(True)
-        self.output_text.setPlaceholderText('--> 按照提示输入网址和时间'
-                                            '\n--> 输入时间(0-23)，用英文逗号分隔，如：16,21,6'
-                                            '\n--> 默认每次执行时间间隔范围为 8-16分钟 不定时')
+        self.output_text.setPlaceholderText('--> 勾选需要启动的应用，该配置可保存'
+                                            '\n--> 输入文件所在位置的正确路径'
+                                            '\n--> 配置在保存后方可生效')
         v_layout.addWidget(self.output_text)
 
 
@@ -171,6 +187,7 @@ class MainWindow(QMainWindow):
 
         # 创建变量，用来保持 worker_thread 工作线程
         self.worker_thread = None
+
     def start(self):
         if self.worker_thread is None or not self.worker_thread.isRunning():
 
@@ -184,31 +201,77 @@ class MainWindow(QMainWindow):
                 self.output_text.append('# --> 待执行任务：打开编译器')
                 idea_path = self.idea_path.text()
 
+            translate_path = None
+            if self.open_translate.isChecked():
+                self.output_text.append('# --> 待执行任务：打开翻译软件')
+                translate_path = self.translate_path.text()
 
-            if not music_path or not idea_path:
+
+            if not music_path and not idea_path and not translate_path:
                 self.output_text.append('# --> 错误：请选择要启动的程序')
                 return
             else:
-                self.worker_thread = WorkerThread(music_path, idea_path)
+                self.worker_thread = WorkerThread(music_path, idea_path, translate_path)
                 self.worker_thread.message_signal.connect(self.update_output)
                 self.worker_thread.start()
 
     def pause(self):
-        if self.worker_thread is None:
-            self.output_text.append('请先开始任务！！！')
+        if self.worker_thread is None or not self.worker_thread.isRunning():
+            self.output_text.append('当前无任务运行！！！')
             return
 
         if self.worker_thread and self.worker_thread.isRunning():
             self.worker_thread.stop()
-            self.output_text.append('停止任务...')
+            self.output_text.append('任务已经停止！！！')
+
+
+    def write_config_path(self, name, app_path):
+        """
+        对修改的路径进行更改并保存到ini配置文件
+        """
+        config = RawConfigParser()
+        config.read(app_config_file_path, encoding='utf-8')
+        # 修改配置软件启动路径
+        config.set('app启动路径', name, app_path)
+        # 写入ini文件
+        with open(app_config_file_path, 'w', encoding='utf-8') as f:
+            config.write(f)
+        self.output_text.append(f'软件：{name}, 路径{app_path}：保存成功')
+
+    def write_config_default(self, name, default):
+        """
+        修改app是否默认开启或关闭
+        """
+        config = RawConfigParser()
+        config.read(app_config_file_path, encoding='utf-8')
+        config.set('app是否默认启动', name, default)
+        with open(app_config_file_path, 'w', encoding='utf-8') as f:
+            config.write(f)
+        self.output_text.append(f'软件：{name}, 状态保存成功--{default}')
+
+    def save_change(self):
+
+        app_paths= {
+                    'music': self.music_path.text(),
+                    'idea': self.idea_path.text(),
+                    'translate': self.translate_path.text()
+                    }
+
+        for key, value in app_paths.items():
+            self.write_config_path(key, value)
+
+        start_or_close = {
+            'music': self.open_music.isChecked(),
+            'idea': self.open_idea.isChecked(),
+            'translate': self.open_translate.isChecked()
+        }
+
+        for key, value in start_or_close.items():
+            self.write_config_default(key, value)
+
 
     def update_output(self, message):
         self.output_text.append(message)
-
-
-
-
-
 
 
 
